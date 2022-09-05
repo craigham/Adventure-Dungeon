a2c646237da65d7c53aeaaf65ce2376f5cddf36a
Craig H <craigham@users.noreply.github.com>
2022-09-05 00:29:10 +0000

    new movement

--- classes.py
+++ classes.py
@@ -1,9 +1,13 @@
 import turtle
+
 screen = turtle.Screen()
 screen.setup(1.0, 1.0)
 
+
 class Map:  # This class uses Turtle to draw a map of rooms
+
     def __init__(self, room_size, n_rooms_axis):
+        self.max_rooms_dimension = n_rooms_axis
         self.turtle = turtle.Turtle()
         self.turtle.speed(0)
         self.turtle.hideturtle()
@@ -14,9 +18,53 @@ class Map:  # This class uses Turtle to draw a map of rooms
             for j in range(n_rooms_axis):
                 self.rooms.append([i - 1, j - 1])
                 self._draw_room([i - 1, j - 1])
+        self.player_location = [0, 0]
+        self.turtle.showturtle()
+        # lets draw the player now
+        self.turtle.color('red')
+        self.turtle.penup()
+        self.turtle.shape('circle')  # change what turtle looks like
+        self.turtle.goto(self.player_location[0] * room_size,
+                         self.player_location[1] * room_size)
+        self.turtle.left(90)
+        # self.turtle.pendown()
 
     def clear_map(self):
         self.turtle.clear()
+
+    def move_player(self, direction):
+        print(f'Attempting to move player: {direction}')
+        print(f'player location is: {self.player_location}')
+        if direction.lower().startswith('n'):
+            if self.player_location[1] > -1:
+                self.turtle.forward(self.room_size)
+                self.player_location[1] -= 1
+            else:
+                print(f"'{direction}'' is not a valid direction.")
+        elif direction.lower().startswith('s'):
+            if self.player_location[1] < self.max_rooms_dimension - 2:
+                self.turtle.left(180)
+                self.turtle.forward(self.room_size)
+                self.turtle.right(180)
+                self.player_location[1] += 1
+            else:
+                print(f"'{direction}'' is not a valid direction.")
+        elif direction.lower().startswith('e'):
+            if self.player_location[0] < self.max_rooms_dimension - 2:
+                self.turtle.right(90)
+                self.turtle.forward(self.room_size)
+                self.turtle.left(90)
+                self.player_location[0] += 1
+            else:
+                print(f"'{direction}'' is not a valid direction.")
+        elif direction.lower().startswith('w'):
+            if self.player_location[0] > -1:
+                self.turtle.left(90)
+                self.turtle.forward(self.room_size)
+                self.turtle.right(90)
+                self.player_location[0] -= 1
+            else:
+                print(f"'{direction}'' is not a valid direction.")
 
     def _draw_room(self, room_loc):
         self.turtle.penup()
@@ -29,7 +77,7 @@ class Map:  # This class uses Turtle to draw a map of rooms
             self.turtle.right(90)
         self.turtle.penup()
 
-    
+
 class Player:
 
     def __init__(self, start_room, map_scale, name, gender):
@@ -38,34 +86,34 @@ class Player:
         self.n_lives = 1
         self.name = name  # player name
         self.gender = gender  # player gender
-        self.t = turtle.Turtle()
+        # self.t = turtle.Turtle()
         self.inventory = []  # to keep track of items
         self.state = 'start room'  # game state
-        self.t.color('red')  # change the colour of the turtle
-        self.t.penup()
-        self.t.shape('circle')  # change what turtle looks like
-        self.t.goto(start_room[0] * map_scale, start_room[1] * map_scale)
-        self.t.left(90)
+        # self.t.color('red')  # change the colour of the turtle
+        # self.t.penup()
+        # self.t.shape('circle')  # change what turtle looks like
+        # self.t.goto(start_room[0] * map_scale, start_room[1] * map_scale)
+        # self.t.left(90)
 
-    def move(self, direction):
-        if direction == 'west':
-            self.t.left(90)
-            self.t.forward(self.map_scale)
-            self.t.right(90)
-            self.loc[0] -= 1
-        elif direction == 'east':
-            self.t.right(90)
-            self.t.forward(self.map_scale)
-            self.t.left(90)
-            self.loc[0] += 1
-        elif direction == 'south':
-            self.t.left(180)
-            self.t.forward(self.map_scale)
-            self.t.right(180)
-            self.loc[1] -= 1
-        elif direction == 'north':
-            self.t.forward(self.map_scale)
-            self.loc[1] += 1
+    # def move(self, direction):
+    #     if direction == 'west':
+    #         self.t.left(90)
+    #         self.t.forward(self.map_scale)
+    #         self.t.right(90)
+    #         self.loc[0] -= 1
+    #     elif direction == 'east':
+    #         self.t.right(90)
+    #         self.t.forward(self.map_scale)
+    #         self.t.left(90)
+    #         self.loc[0] += 1
+    #     elif direction == 'south':
+    #         self.t.left(180)
+    #         self.t.forward(self.map_scale)
+    #         self.t.right(180)
+    #         self.loc[1] -= 1
+    #     elif direction == 'north':
+    #         self.t.forward(self.map_scale)
+    #         self.loc[1] += 1
 
     def add_inventory(self, item):
         self.inventory.append(item)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from py_fight import PyFight
 from random import randint
 import time
 
+
 def start_game():
     debug = False
     room_size = 100
@@ -32,112 +33,118 @@ def start_game():
         print(text)
         choice = input('Which way should ' + name + ' go?\n')
 
+    print('entering while loop')
     while choice != 'quit' and player.n_lives > 0:
-        prompt = 'What will ' + name + ' do next?\n'
-        if debug:
-            if 'life orb' in choice:
-                player.move('south')
-                player.move('west')
-                player.state = ('southwest room')
-                print('Went southwest to Life Orb')
-            elif 'py fight 2' in choice:
-                print('Skipped to Py Fight 2')
-            elif 'py fight 1' in choice:
-                player.state = 'north room'
-                player.move('north')
-                choice = 'enter'
-                print('Skipped to Py Fight 1')
-        if player.state == 'start room':
-            if 'north' in choice:
-                if debug:
-                    print('Went north')
-                player.move('north')
-                player.state = 'north room'
-                text = '\n' + name + ' sees a door'
-            elif 'south' in choice:
-                if debug:
-                    print('Went south')
-                player.move('south')
-                player.state = 'south room'
-                text = name + ' sees a chest a few steps away.'
-            elif 'east' in choice:
-                if debug:
-                    print('Went east')
-                player.move('east')
-                player.state = 'east room'
-                text = name + ' is in the west room\n'
-            elif 'west' in choice:
-                if debug:
-                    print('Went west')
-                player.move('west')
-                player.state = 'west room'
-                text = name + ' is in the west room\n'
-            else:
-                print('\nThat wasn\'t an option. Try again:')
-        elif player.state == 'north room':
-            if 'south' in choice:
-                player.move('south')
-                player.state = 'start room'
-                text = name + ' is in the room ' + gender.lower() + 'started from.'
-            elif 'enter' in choice or 'open' in choice or 'kick' in choice or 'knock' in choice or 'ram' in choice or 'door' in choice:
-                print(name + ' creaks open the door to discover a giant python! It looks ready for a fight!')
-                my_map.clear_map()
-                fight = PyFight(name, gender)
-                fight.py_den()
-                fight.python()
-                fight.py_fight(name, gender, player)
-                break
-            elif 'west' in choice:
-                player.move('west')
-                player.state = 'northwest room'
-                text = 'You are in the northwest room'
-        elif player.state == 'south room':
-            if 'north' in choice:
-                player.move('north')
-                player.state = 'start room'
-                text = name + ' is in the room ' + gender.lower() + ' started from.'
-            elif 'open' in choice:
-                text = name + ' opens the treasure chest to discover a weird shiny object that looked like a big necklace. ' + name + ' puts it on, but quickly realizes that it is too heavy. ' + name + ' just puts it in ' + gender2 + ' backpack'
-                player.add_inventory('Strength Necklace')
-        elif player.state == 'west room':
-            if 'east' in choice:
-                player.move('east')
-                player.state = 'start room'
-                text = name + ' is in the room ' + gender.lower() + ' started from.'
-            elif 'north' in choice:
-                player.move('north')
-                player.state = 'northwest room'
-                text = name + 'is in the northwest room.'
-            elif 'south' in choice:
-                if debug:
-                    print('Went south')
-                player.move('south')
-                player.state = 'southwest room'
-                text = name + ' spots a glowing orange orb in the center of the room.'
-        elif player.state == 'southwest room':
-            if 'north' in choice:
-                player.move('north')
-                player.state = 'west room'
-                text = name + 'is in the west room'
-            elif 'grab' in choice:
-                if randint(1, 2) == 2:
-                    player.n_lives += 1
-                    print('You got lucky and gained a life.')
-                else:
-                    player.n_lives -= 1
-                    print('You got unlucky and lost a life.')
-                print(f'Player life is: {player.n_lives}')
-            elif 'east' in choice:
-                player.move('east')
-                player.state = 'south room'
-                text = name + ' sees a chest a few steps away.'
-        elif player.state == 'northwest room':
-            if 'south' in choice:
-                player.move('south')
-                player.state = 'west room'
-                text = name + 'is in the west room'
-        else:
-            print('\nThat wasn\'t an option. Try again:')
-        print(text)
+
+        prompt = 'Which door would you like to enter?\n'
+        print(f'user entered: {choice}')
+        my_map.move_player(choice)
+        # if debug:
+        #     if 'life orb' in choice:
+        #         player.move('south')
+        #         player.move('west')
+        #         player.state = ('southwest room')
+        #         print('Went southwest to Life Orb')
+        #     elif 'py fight 2' in choice:
+        #         print('Skipped to Py Fight 2')
+        #     elif 'py fight 1' in choice:
+        #         player.state = 'north room'
+        #         player.move('north')
+        #         choice = 'enter'
+        #         print('Skipped to Py Fight 1')
+        # if player.state == 'start room':
+        #     if 'north' in choice:
+        #         if debug:
+        #             print('Went north')
+        #         player.move('north')
+        #         player.state = 'north room'
+        #         text = '\n' + name + ' sees a door'
+        #     elif 'south' in choice:
+        #         if debug:
+        #             print('Went south')
+        #         player.move('south')
+        #         player.state = 'south room'
+        #         text = name + ' sees a chest a few steps away.'
+        #     elif 'east' in choice:
+        #         if debug:
+        #             print('Went east')
+        #         player.move('east')
+        #         player.state = 'east room'
+        #         text = name + ' is in the west room\n'
+        #     elif 'west' in choice:
+        #         if debug:
+        #             print('Went west')
+        #         player.move('west')
+        #         player.state = 'west room'
+        #         text = name + ' is in the west room\n'
+        #     else:
+        #         print('\nThat wasn\'t an option. Try again:')
+        # elif player.state == 'north room':
+        #     if 'south' in choice:
+        #         player.move('south')
+        #         player.state = 'start room'
+        #         text = name + ' is in the room ' + gender.lower() + 'started from.'
+        #     elif 'enter' in choice or 'open' in choice or 'kick' in choice or 'knock' in choice or 'ram' in choice or 'door' in choice:
+        #         print(name + ' creaks open the door to discover a giant python! It looks ready for a fight!')
+        #         my_map.clear_map()
+        #         fight = PyFight(name, gender)
+        #         fight.py_den()
+        #         fight.python()
+        #         fight.py_fight(name, gender, player)
+        #         break
+        #     elif 'west' in choice:
+        #         player.move('west')
+        #         player.state = 'northwest room'
+        #         text = 'You are in the northwest room'
+        # elif player.state == 'south room':
+        #     if 'north' in choice:
+        #         player.move('north')
+        #         player.state = 'start room'
+        #         text = name + ' is in the room ' + gender.lower() + ' started from.'
+        #     elif 'open' in choice:
+        #         text = name + ' opens the treasure chest to discover a weird shiny object that looked like a big necklace. ' + name + ' puts it on, but quickly realizes that it is too heavy. ' + name + ' just puts it in ' + gender2 + ' backpack'
+        #         player.add_inventory('Strength Necklace')
+        # elif player.state == 'west room':
+        #     if 'east' in choice:
+        #         player.move('east')
+        #         player.state = 'start room'
+        #         text = name + ' is in the room ' + gender.lower() + ' started from.'
+        #     elif 'north' in choice:
+        #         player.move('north')
+        #         player.state = 'northwest room'
+        #         text = name + 'is in the northwest room.'
+        #     elif 'south' in choice:
+        #         if debug:
+        #             print('Went south')
+        #         player.move('south')
+        #         player.state = 'southwest room'
+        #         text = name + ' spots a glowing orange orb in the center of the room.'
+        # elif player.state == 'southwest room':
+        #     if 'north' in choice:
+        #         player.move('north')
+        #         player.state = 'west room'
+        #         text = name + 'is in the west room'
+        #     elif 'grab' in choice:
+        #         if randint(1, 2) == 2:
+        #             player.n_lives += 1
+        #             print('You got lucky and gained a life.')
+        #         else:
+        #             player.n_lives -= 1
+        #             print('You got unlucky and lost a life.')
+        #         print(f'Player life is: {player.n_lives}')
+        #     elif 'east' in choice:
+        #         player.move('east')
+        #         player.state = 'south room'
+        #         text = name + ' sees a chest a few steps away.'
+        # elif player.state == 'northwest room':
+        #     if 'south' in choice:
+        #         player.move('south')
+        #         player.state = 'west room'
+        #         text = name + 'is in the west room'
+        # else:
+        #     print('\nThat wasn\'t an option. Try again:')
+        # print(text)
         choice = input(prompt).lower()
+
+
 start_game()
